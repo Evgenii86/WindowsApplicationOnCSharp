78f0301e14ce2f2e94a09edccf1746187d5eb340
Evgenii86 <73522392+Evgenii86@users.noreply.github.com>
2021-02-04 13:47:26 +0300

    Add files via upload

---
+++ LabsWindowsForms/ITMO.LabsWindCS.Demo2/ITMO.LabsWindCS.Demo2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ITMO.LabsWindCS.Demo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1530, 814);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LabsWindowsForms/ITMO.LabsWindCS.Demo2/ITMO.LabsWindCS.Demo2/nForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.LabsWindCS.Demo2
+{
+    public partial class nForm : ITMO.LabsWindCS.Demo2.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
